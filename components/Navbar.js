59413f4bcafffc70434c2b47e5c720984bdd8697
BossFreind1234 <vincentredtube@gmail.com>
2022-09-25 17:16:20 -0400

    Blog Site

--- components/Navbar.js
+++ components/Navbar.js
@@ -25,6 +25,9 @@ const Navbar = () => {
                             <a href="../contact" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
                         </li>
                         <li>
+                            <a href="../blogs" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Blog</a>
+                        </li>
+                        <li>
                             <a href="https://github.com/CertifiedRice/website-certifiedrice" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Source</a>
                         </li>
                     </ul>
---
+++ posts/test.md
@@ -0,0 +1,9 @@
+---
+title: 'Testing'
+date: '9/25/22'
+excerpt: 'Testing for the Blog posts feature for my website!'
+---
+
+
+### Testing
+Hello World!
