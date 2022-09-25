71b344c61bf9ade3e3df60c45687fe9bd343541b
BossFreind1234 <vincentredtube@gmail.com>
2022-09-24 20:38:02 -0400

    head

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,13 @@
 import Navbar from "./Navbar";
+import Head from "next/head";
 
-const Layout = ({children}) => {
+const Layout = ({ children }) => {
     return (
         <div className="content">
+            <Head>
+                <link rel="icon" href="https://avatars.githubusercontent.com/certifiedrice" />
+                <title>CertifiedRice</title>
+            </Head>
             <Navbar />
             {children}
             {/* <Foot /> */}
