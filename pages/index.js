2497135b6845de1c8ddaab85d8f0b87a26c135fd
BossFreind1234 <vincentredtube@gmail.com>
2022-09-29 18:45:14 -0400

    removed button from 404.js

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,7 @@ export default function Home() {
 
           <div class="flex space-x-2 justify-center">
             <a href="./about">
-              <button type="button" class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Read More!</button>
+              <button type="button" class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Read More!</button>
             </a>
           </div>
         </div>
