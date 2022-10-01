2fc1ba84e8e2cddf172c4928efeee5a2cc3189cb
BossFreind1234 <vincentredtube@gmail.com>
2022-10-01 18:40:06 -0400

    fixed OS light theme issue

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,8 @@
 const Footer = () => {
     return (
-        <footer class="p-4 bg-white rounded-lg shadow md:px-6 md:py-8 dark:bg-gray-900">
+        <footer class="p-4 rounded-lg shadow md:px-6 md:py-8 bg-gray-900">
             <div class="sm:flex sm:items-center sm:justify-between">
-                <ul class="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
+                <ul class="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 text-gray-400">
                     <li>
                         <a href="../about" class="mr-4 hover:underline md:mr-6 ">About</a>
                     </li>
@@ -17,8 +17,8 @@ const Footer = () => {
                     </li>
                 </ul>
             </div>
-            <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-            <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022-Present <a href="https://github.com/CertifiedRice" class="hover:underline">CertifiedRice</a>. All Rights Reserved.
+            <hr class="my-6 border-gray-200 sm:mx-auto  border-gray-700 lg:my-8" />
+            <span class="block text-sm text-gray-500 sm:text-center  text-gray-400">© 2022-Present <a href="https://github.com/CertifiedRice" class="hover:underline">CertifiedRice</a>. All Rights Reserved.
             </span>
         </footer>
     )
